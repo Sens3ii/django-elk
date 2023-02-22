3acaa7c634725bf6c84dfe12f4cc82abe35880c2
daniil <densteeper@gmail.com>
2023-02-22 12:13:04 +0600

    initial commit

---
+++ api/views.py
@@ -0,0 +1,31 @@
+# Create your views here.
+from api.serializers import UserSerializer, GroupSerializer
+from django.contrib.auth.models import User, Group
+from rest_framework import permissions
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+
+@api_view(['GET', 'POST'])
+def hello(request):
+    return Response({'message': 'Hello, World!'})
